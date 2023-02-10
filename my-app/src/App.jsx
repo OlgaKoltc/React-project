10f8cac612f53b028094c027c3ac839529f34c8e
OlgaKoltc <Olga.Koltc@gmail.com>
2023-02-10 16:09:27 +0300

    add project structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css';
+import Backside from './components/Backside';
+import Footer from './components/Footer';
+import Frontside from './components/Frontside';
+import Header from './components/Header';
+import Main from './components/Main';
+import Tema from './components/Tema';
 
 function App() {
   return (
     <div className="App">
-
+<Header>
+  <Tema></Tema>
+</Header>
+<Main>
+  <Card>
+    <Frontside></Frontside>
+    <Backside></Backside>
+  </Card>
+</Main>
+<Footer></Footer>
     </div>
   );
 }
---
+++ my-app/src/components/Frontside.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Frontside() {
+  return (
+    <div>Frontside</div>
+  )
+}
