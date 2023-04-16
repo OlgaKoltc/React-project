614b60e0e29d545d6fbcaf9113aea5b405e41479
OlgaKoltc <Olga.Koltc@gmail.com>
2023-04-16 19:56:44 +0300

    add context

--- my-app/src/pages/PageHome/Table.jsx
+++ my-app/src/pages/PageHome/Table.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import word from "../../wordsList.json";
+import React, { useContext, useEffect, useState } from "react";
+import { glContext } from "../../Context/MyContext";
+
+//import word from "../../wordsList.json";
 import st from "./style.module.scss";
 import TableRow from "../../components/TableRow/TableRow";
 
 export default function Table() {
+  const { data, setData } = useContext(glContext);
+
   return (
     <div>
       <div className={st.container_table}>
-        {word.map((item) => (
+        {data.map((item) => (
           <TableRow key={item.id} item={item} />
         ))}
       </div>
